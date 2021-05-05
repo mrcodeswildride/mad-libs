961f57db2aff9f303c257e025b6dae9bc5ec6324
David Keller <kellerdb@gmail.com>
2021-05-05 13:00:29 -0700

    formatting

--- script.js
+++ script.js
@@ -6,21 +6,20 @@ let place = document.getElementById(`place`)
 
 storyButton.addEventListener(`click`, showStory)
 
+thing.focus()
+
 function showStory() {
   let thingValue = thing.value.trim()
   let numberValue = number.value.trim()
   let placeValue = place.value.trim()
 
   if (thingValue == ``) {
-    storyParagraph.innerHTML = `Please type a thing.`
-  }
-  else if (numberValue == ``) {
-    storyParagraph.innerHTML = `Please type a number.`
-  }
-  else if (placeValue == ``) {
-    storyParagraph.innerHTML = `Please type a place.`
-  }
-  else {
+    storyParagraph.innerHTML = `Type a thing.`
+  } else if (numberValue == ``) {
+    storyParagraph.innerHTML = `Type a number.`
+  } else if (placeValue == ``) {
+    storyParagraph.innerHTML = `Type a place.`
+  } else {
     storyParagraph.innerHTML = `A man bought a ${thingValue} at the grocery store for ${numberValue} dollars and brought it to the ${placeValue}.`
   }
 }
